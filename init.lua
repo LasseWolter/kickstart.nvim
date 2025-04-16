be36bd1aa6168e2c7863ff5204d605eebc4130d5
Lasse Wolter <wolter.lasse@gmail.com>
2025-04-16 22:34:00 +0100

    chore: Comment out autotag for now because it raises warning

--- init.lua
+++ init.lua
@@ -852,23 +852,23 @@ require('lazy').setup({
       }
     end,
   },
-  {
-    'windwp/nvim-ts-autotag',
-    opts = {
-      -- Defaults
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false, -- Auto close on trailing </
-    },
-    -- Also override individual filetype configs, these take priority.
-    -- Empty by default, useful if one of the "opts" global settings
-    -- doesn't work well in a specific filetype
-    -- per_filetype = {
-    --   ['html'] = {
-    --     enable_close = false,
-    --   },
-    -- },
-  },
+--  {
+--    'windwp/nvim-ts-autotag',
+--    opts = {
+--      -- Defaults
+--      enable_close = true, -- Auto close tags
+--      enable_rename = true, -- Auto rename pairs of tags
+--      enable_close_on_slash = false, -- Auto close on trailing </
+--    },
+--    -- Also override individual filetype configs, these take priority.
+--    -- Empty by default, useful if one of the "opts" global settings
+--    -- doesn't work well in a specific filetype
+--    -- per_filetype = {
+--    --   ['html'] = {
+--    --     enable_close = false,
+--    --   },
+--    -- },
+--  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
