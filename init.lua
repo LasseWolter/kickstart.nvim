d303331cfd454e46530224701b1ff87f81eb7c8f
Lasse <wolter.lasse@gmail.com>
2025-09-24 20:05:40 +0100

    feat: make live grep fuzzy

--- init.lua
+++ init.lua
@@ -303,7 +303,6 @@ require('lazy').setup({
       end, { desc = '[S]earch [All] files' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sv', builtin.git_files, { desc = '[S]earch [V]ersion control' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -321,12 +320,14 @@ require('lazy').setup({
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.grep_string {
+          only_sort_text = true,
+          path_display = { 'smart' },
+          word_match = "-w",
+          search = '',
         }
-      end, { desc = '[S]earch [/] in Open Files' })
+      end, { desc = '[S]earch by [G]rep' })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
