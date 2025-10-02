98c80f5d366e492546b423db717e42eeb8d76622
Lasse <wolter.lasse@gmail.com>
2025-10-02 14:27:54 +0100

    feat: separate grep search and fuzzy search

--- init.lua
+++ init.lua
@@ -365,6 +365,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>gb', builtin.git_branches)
+      vim.keymap.set('n', '<leader>sg', builtin.grep_string, { desc = '[S]earch by [G]rep (non-fuzzy)' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -377,14 +378,14 @@ require('lazy').setup({
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>sg', function()
+      vim.keymap.set('n', '<leader>sz', function()
         builtin.grep_string {
           only_sort_text = true,
           path_display = { 'smart' },
           word_match = '-w',
           search = '',
         }
-      end, { desc = '[S]earch by [G]rep' })
+      end, { desc = '[S]earch by [G]rep (fuzzy)' })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
